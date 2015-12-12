738b2d963163ba169f49f607d50bda24df53b988
Andreas Magnusson <andreas.magnusson90@gmail.com>
2015-12-12 18:03:37 +0100

    Started making the changes to implement FishShoal.

--- code/Aquarium.m
+++ code/Aquarium.m
@@ -1,52 +1,28 @@
 classdef Aquarium < handle
     
     properties
-        inhabitants
-        positions
+        shark
         tankSize
-        fishcalculation
+        fishShoal
     end
     
     methods
-        function obj = Aquarium(shark_consts, tank_consts, fish_consts, weights, beta);
-            obj.positions = zeros(tank_consts.tankSize);
+        function obj = Aquarium(shark_consts, tank_consts, fish_consts, weights, beta)
             obj.tankSize = tank_consts.tankSize;
             % Create shark
             shark_consts.position = ...
                 randi(round(0.25*obj.tankSize),1,2) + round(0.5*obj.tankSize);
             shark_consts.tankSize = tank_consts.tankSize;
-            shark = Shark(shark_consts, weights, beta);
-            obj.inhabitants{1} = shark;
-            obj.positions(shark.position(1),shark.position(2)) = 1;
+            obj.shark = Shark(shark_consts, weights, beta);
             % Create fish shoal
-            obj.fishcalculation = FishCalculation(tank_consts.tankSize);
-            fishVelocity = randi(fish_consts.maxSpeed,1,2).*(1-2*(randi(2,1,2)-1));
-            while norm(fishVelocity) > fish_consts.maxSpeed
-                fishVelocity = randi(fish_consts.maxSpeed,1,2).*(1-2*(randi(2,1,2)-1));
-            end
-            for i=2:tank_consts.nrOfFish+1
-                pos = randi(round(tank_consts.tankSize/4),1,2);
-                while obj.positions(pos(1),pos(2)) ~= 0
-                    pos = randi(round(tank_consts.tankSize/4),1,2);
-                end
-                obj.inhabitants{i} = Fish(pos,fishVelocity,obj.tankSize);
-                obj.positions(pos(1),pos(2)) = i;
-            end
+            obj.fishShoal = FishShoal(tank_consts.tankSize, tank_consts.nrOfFish, fish_consts.maxSpeed);
         end
         
         function fitness = run(obj,draw)
-            while obj.inhabitants{1}.hunting
-                if rand < 0.1
-                    mspeed = obj.inhabitants{2}.maxSpeed;
-                    fishVelocity = randi(mspeed,1,2).*(1-2*(randi(2,1,2)-1));
-                    while norm(fishVelocity) > mspeed
-                        fishVelocity = randi(mspeed,1,2).*(1-2*(randi(2,1,2)-1));
-                    end
-                    for i=2:length(obj.inhabitants)
-                        obj.inhabitants{i}.velocity = fishVelocity;
-                    end
-                end
-                pos = zeros(obj.tankSize);
+            while obj.shark.hunting
+                
+                obj.shark.updatePosition(obj.fishShoal.fishes);
+                
                 for i=1:length(obj.inhabitants)
                     newPos = obj.inhabitants{i}.updatePosition(obj.positions);
                     if i==1 && obj.positions(newPos(1), newPos(2)) > 1; % Kill fish
