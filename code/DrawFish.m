feecf6e291304adc47456a2eb2f2d8d5ebc6c17b
Kristian Onsjö <kristian.onsjo@gmail.com>
2015-12-13 00:49:09 +0100

    DrawFish finished. Support single frame figure, as well as movie recording. Also supports eporting capabilities for both options.

--- code/DrawFish.m
+++ code/DrawFish.m
@@ -1,31 +1,66 @@
-% VERY WIP!
-classdef DrawFish % < Aquarium
+classdef DrawFish
  
-methods
- function obj = DrawFish(tankSize,positions,inhabitants)
-  if(nargin == 0)
-   tankSize = 100;
-   positions = randi(tankSize,51,2);
-   inhabitants = 1:size(positions,2);
-  end
-%  obj@Aquarium(tankSize,positions,inhabitants);
-  obj.tankSize = tankSize;
-  obj.positions = positions;
-  obj.inhabitants = inhabitants;
- end
- 
- function drawMap()
-  obj.graphicHandle = figure;
-  hold on
-  plot(obj.positions(1,1),obj.positions(2,1),'*b') % Plot shark.
-  for i=2:size(obj.positions,1)
-   if(obj.inhabitants(i)~=0)
-    plot(obj.positions(1,i),obj.positions(2,i),'.r') % Plot living fish.
-   end
-  end
-  axis([0 obj.tankSize 0 obj.tankSize])
-  hold off
- end
+properties
+ movieFrames
+ aq
+ figIndex
 end
  
+methods
+ % Use an object of class Aquarium as input.
+ function obj = DrawFish(aquarium) % Constructor.
+  obj.movieFrames = [];
+  obj.aq = aquarium;
+  obj.figIndex = 1;
+ end
+ 
+ function stillfig(obj) % Draw figure of current Aquarium.
+  % Usage: thisObject.stillfig in loop with Aquarium.
+  figure(1)
+  N = length(obj.aq.fishShoal.fishes);
+  coordinates = zeros(N,2);
+  for i = 1:N
+   coordinates(i,:) = obj.aq.fishShoal.fishes(i).position;
+  end
+  plot(obj.aq.shark.position(1),obj.aq.shark.position(2),'r.','markersize',20)
+  hold on
+  plot(coordinates(1:end,1),coordinates(1:end,2),'.','markersize',10)
+  axis([0 obj.aq.tankSize 0 obj.aq.tankSize])
+  hold off
+  drawnow
+ end
+ 
+ function obj = exportFigure(obj) % Export current Aquarium.
+  % Usage: thisObject = thisObject.exportFigure after stillfig.
+  exportPath = sprintf('../data/figure%i.png',obj.figIndex);
+  obj.figIndex = obj.figIndex + 1;
+  print(exportPath,'-dpng')
+ end
+ 
+ function obj = animate(obj) % Record movie.
+  % Usage: thisObject = thisObject.animate in loop with Aquarium.
+  movieHandle = figure(1);
+  N = length(obj.aq.fishShoal.fishes);
+  coordinates = zeros(N,2);
+  for i = 1:N
+   coordinates(i,:) = obj.aq.fishShoal.fishes(i).position;
+  end
+  plot(obj.aq.shark.position(1),obj.aq.shark.position(2),'r.','markersize',20)
+  hold on
+  plot(coordinates(1:end,1),coordinates(1:end,2),'.','markersize',10)
+  axis([0 obj.aq.tankSize 0 obj.aq.tankSize])
+  hold off
+  drawnow
+  obj.movieFrames = [obj.movieFrames getframe(movieHandle)];
+ end
+ 
+ function exportMovie(obj) % Export recorded movie.
+  % Usage: thisObject.exportMovie after evaluation loop.
+  exportPath = sprintf('../data/movie.avi');
+  v = VideoWriter(exportPath);
+  open(v)
+  writeVideo(v,obj.movieFrames)
+  close(v)
+ end
+end
 end
