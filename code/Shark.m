074ee666efbfcd54f1c6eb19e2e5fc4b1522d29d
Henrik Adolfsson <zteifel@gmail.com>
2015-12-13 09:21:13 +0100

    fix how energy is calculated

--- code/Shark.m
+++ code/Shark.m
@@ -5,7 +5,7 @@ classdef Shark < Animal
     observeDist
     tankSize
     brain
-    maxSpeed 
+    maxSpeed
     fishEatGoal
     energy = 0;
     maxEnergy
@@ -28,13 +28,13 @@ classdef Shark < Animal
         dir = [0,0];
         while sum(abs(dir)) < 1
             dir = randi(3,1,2)-2;
-        end 
-        obj.direction = dir; 
+        end
+        obj.direction = dir;
         if length(weights) == 0
             obj.brain = WalnutBrain(0.5,0.5);
         else
             obj.brain = AIBrain(weights,beta);
-        end 
+        end
     end
 
     function newPos = updatePosition(obj, tank)
@@ -45,11 +45,11 @@ classdef Shark < Animal
       fishInFront = obj.fishInAngle(pi/2);
       fishInBack = ~fishInFront;
       fishInScope = {fishInFront, fishInBack};
-    
+
       for i=1:length(fishInScope)
         fish = bigTank;
         fish(~and(fishInObs,fishInScope{i})) = 0; % Rm all not in scope or dist
-        center = obj.massCenter(fish);            % Get relative center of shoal  
+        center = obj.massCenter(fish);            % Get relative center of shoal
         if sum(isnan(center)) > 0 || isequal(center,[0,0])
           angle = 0.5;
           dist = 0;
@@ -65,11 +65,11 @@ classdef Shark < Animal
       close_fish( ...
         ~and(obj.fishInObserveDist(obj.maxSpeed), ...
              obj.fishInAngle(obj.moveAngle)) ) = 0;
-      [close_dists, close_angles] = obj.fishClose(close_fish);  
+      [close_dists, close_angles] = obj.fishClose(close_fish);
       brainInputs(3) = close_dists;
       brainInputs(6) = close_angles;
 
-      [normSpeed, normDir] = obj.brain.getMovement(brainInputs'); 
+      [normSpeed, normDir] = obj.brain.getMovement(brainInputs');
       [newPos, newDir] = obj.newPosition(normSpeed,normDir,obj.moveAngle);
 
       obj.position = newPos;
@@ -77,8 +77,8 @@ classdef Shark < Animal
 
       if tank(obj.position(1), obj.position(2)) > 1
         obj.fishEaten = obj.fishEaten + 1;
-      end 
-      obj.energy = obj.energy + normSpeed;
+      end
+      obj.energy = obj.energy + ( floor(velocity*obj.maxSpeed)+1 );
       if obj.fishEaten >= obj.fishEatGoal || obj.energy >= obj.maxEnergy
         if obj.energy >= obj.maxEnergy
           obj.energy = obj.maxEnergy;
@@ -100,8 +100,8 @@ classdef Shark < Animal
       for i=1:2
         if P(i) > obj.tankSize
           p(i) = P(i) - obj.tankSize;
-        elseif P(i) <= 0 
-          p(i) = P(i) + obj.tankSize; 
+        elseif P(i) <= 0
+          p(i) = P(i) + obj.tankSize;
         else
           p(i) = P(i);
         end
@@ -111,16 +111,16 @@ classdef Shark < Animal
 
     function [dists, angles] = fishClose(obj,fish)
       angle = obj.moveAngle;
-      nr = obj.closeObserve; 
+      nr = obj.closeObserve;
       if sum(sum(fish)) == 0
         dists = zeros(1,nr);
         angles = 0.5*ones(1,nr);
         return
       end
-      gs = 3*obj.tankSize; 
+      gs = 3*obj.tankSize;
       iX = (1:gs)'*ones(1,gs);
       iY = ones(gs,1)*(1:gs);
-      xs = iX(fish)-(obj.position(1)+obj.tankSize); 
+      xs = iX(fish)-(obj.position(1)+obj.tankSize);
       ys = iY(fish)-(obj.position(2)+obj.tankSize);
 
       dists = arrayfun( @(x,y) 1-norm([x,y])/obj.maxSpeed, xs,ys);
@@ -130,7 +130,7 @@ classdef Shark < Animal
       angles = arrayfun( @(x,y) ...
         2*acos(dot([x,y],sharkBase')/(norm([x,y])*norm(sharkBase)))/pi,xs,ys);
       angles = angles(iSort);
-      angles = angles(1:nr); 
+      angles = angles(1:nr);
     end
 
     function angle = normAngle(obj,center)
@@ -138,7 +138,7 @@ classdef Shark < Animal
         sharkBase = [0,1;-1,0]*obj.direction';
         angle = acos(dot(center,sharkBase')/(norm(center)*norm(sharkBase)))/pi;
     end
-   
+
     % Return position relative to shark
     function pos = massCenter(obj,shoal)
       gs = obj.tankSize * 3;
@@ -152,23 +152,24 @@ classdef Shark < Animal
     function iFish = fishInAngle(obj,angle)
       s = obj.position + obj.tankSize;
       d = obj.direction;
-      gs = 3*obj.tankSize; 
+      gs = 3*obj.tankSize;
       iX = (1:gs)'*ones(1,gs)-s(1);
       iY = ones(gs,1)*(1:gs)-s(2);
-        
+
       iFish = arrayfun( @(x,y) ...
         sign(angle)*acos(dot([x,y],d')/(norm([x,y])*norm(d))) <= angle,iX,iY);
     end
 
     function iFish = fishInObserveDist(obj, dist)
-      gs = 3*obj.tankSize; 
+      gs = 3*obj.tankSize;
       iX = (1:gs)'*ones(1,gs);
       iY = ones(gs,1)*(1:gs);
       X = obj.position(1) + obj.tankSize;
-      Y = obj.position(2) + obj.tankSize; 
+      Y = obj.position(2) + obj.tankSize;
       iFish = arrayfun( @(x,y) ...
         sqrt((X-x)^2 + (Y-y)^2) <= dist,iX, iY);
     end
 
   end
 end
+
