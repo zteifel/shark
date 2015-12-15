7ef66d3dcdc06ee3f4e40a3e12f5515eba55c009
johanamagnusson <andreas.magnusson90@gmail.com>
2015-12-15 11:24:31 +0100

    Commented out figure update.

--- code/Aquarium.m
+++ code/Aquarium.m
@@ -4,7 +4,7 @@ classdef Aquarium < handle
     shark
     positions
     tankSize
-    fig
+    %fig
   end
 
   methods
@@ -19,7 +19,7 @@ classdef Aquarium < handle
       positions = zeros(obj.tankSize);
       positions(fishPos(1),fishPos(2)) = 2;
       obj.positions = positions; 
-      obj.fig = obj.initFig();
+      %obj.fig = obj.initFig();
     end
 
     function fig = initFig(obj)
@@ -72,7 +72,7 @@ classdef Aquarium < handle
       while obj.shark.hunting 
         % Update shark position
         newPos = obj.shark.updatePosition(obj.positions);
-        obj.updateFig();
+        %obj.updateFig();
         if obj.positions(newPos(1),newPos(2)) > 1
             obj.positions(newPos(1),newPos(2)) = 0;
             fishPos = obj.posNotCloseToShark(newPos);
