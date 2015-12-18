251dd5740936686f5ce0db6819d0ca224a903b3c
Andreas Magnusson <andreas.magnusson90@gmail.com>
2015-12-18 18:30:25 +0100

    Changed made when making new FishShoal class.

--- test/FishShoal.m
+++ test/FishShoal.m
@@ -6,7 +6,11 @@ classdef FishShoal
         fishes;
         driftSpeed;
         maxSpeed;
-        
+        dv;
+        shoalSize;
+        attractionDistance;
+        r1;
+        r2;
         
     end
     
@@ -14,9 +18,7 @@ classdef FishShoal
     
     properties (Constant = true)
         
-        r1 = 1;
-        r2 = 10;
-        r3 = 30;
+        
         w = 30; %dead angle omega
         frontOffset = 0; % KEEP AT 0!
         
@@ -25,10 +27,17 @@ classdef FishShoal
     
     methods
         
-        function obj = FishShoal(tankSize, shoalSize, driftSpeed, maxSpeed, scareDistance)
+        function obj = FishShoal(tankSize, shoalSize, driftSpeed, maxSpeed, ...
+                scareDistance, attractionDistance)
+            
             obj.T = tankSize;
             obj.driftSpeed = driftSpeed;
             obj.maxSpeed = maxSpeed;
+            obj.dv = driftSpeed*0.2;
+            obj.shoalSize = shoalSize;
+            obj.attractionDistance = attractionDistance;
+            obj.r1 = attractionDistance / 5;
+            obj.r2 = attractionDistance / 2;
             
             m = tankSize/2 - 4.5;
             n = tankSize/2 + 4.5;
@@ -51,13 +60,12 @@ classdef FishShoal
             dp.y = zeros(N,N);
             dp.theta = zeros(N,N);
             beta = zeros(N,N);
-            front4 = zeros(1,4);
             
             positions = zeros(length(obj.fishes),2);
             for i = 1:length(obj.fishes)
                 positions(i,:) = obj.fishes(i).positionReal;
             end
-            %             averagePosition = mean(positions);
+            averagePosition = mean(positions);
             %calculate distances between fish
             %--------------------------------------------------------------------------
             
@@ -98,97 +106,81 @@ classdef FishShoal
                     if d(i,j) >= obj.r1 && d(i,j) < obj.r2
                         beta(i,j) = npi2pi(d_theta);
                     end
-                    %attraction zone (r2 <= d < r3)
-                    if d(i,j) >= obj.r2 && d(i,j) < obj.r3
+                    %attraction zone (r2 <= d < attractionDistance)
+                    if d(i,j) >= obj.r2 && d(i,j) < obj.attractionDistance
                         beta(i,j) = npi2pi(dp.theta(i,j) - thetai);
                     end
                 end
             end
             
             % beta = transpose(rad2deg(atan2(velocities(:,1),velocities(:,2))*ones(N))) - ...
-                rad2deg(atan2(velocities(:,1),velocities(:,2))*ones(N));
+            % rad2deg(atan2(velocities(:,1),velocities(:,2))*ones(N));
             
             
+            %distance priority - average closest 4 neighbors (with dead zone)
             
-            %front priority - average frontmost 4 neighbors (with dead zone)
+            %find indices of closest 4 neighbors
             temp = zeros(1,N-1);
             for i = 1:N
                 
-                % Far from center
-                %                 if (norm(averagePosition - obj.fishes(i).positionReal) > obj.T/4)
-                %                     theta_new = rad2deg(atan2(averagePosition(2) - obj.fishes(i).positionReal(2),...
-                %                         averagePosition(1) - obj.fishes(i).positionReal(1)));
-                %                     velocityReal = obj.maxSpeed*[cosd(theta_new) sind(theta_new)];
-                %                 else
-                
-                index = 0;
-                diff_front = abs(dp.theta(i,:));
-                %remove self
-                for j = 1:N
-                    if j ~= i
-                        index = index + 1;
-                        temp(index) = diff_front(j);
-                    end
-                end
-                diff_front = temp;
-                sorted = sort(abs(diff_front-obj.frontOffset));
-                
-                %sorted = sort(abs(dp.theta(i,:)));
-                
-                
-                
-                index = 0;
-                min_i = 1; %index into sorted array that yields frontmost dp.theta
-                for k = 1:N-1
+                %                 Far from center
+                if (norm(averagePosition - obj.fishes(i).positionReal) > obj.attractionDistance)
+                    theta_new = rad2deg(atan2(averagePosition(2) - obj.fishes(i).positionReal(2),...
+                        averagePosition(1) - obj.fishes(i).positionReal(1)));
+                    velocityReal = obj.driftSpeed*[cosd(theta_new) sind(theta_new)];
+                else
+                    
+                    index = 0;
+                    diff_p = d(i,:);
+                    %remove self
                     for j = 1:N
-                        
-                        %remove dead zone, searching zone, and self, and limit to 4
-                        if index < 4 && j ~= i && dp.theta(i,j) < (180 - obj.w + obj.frontOffset) ...
-                                && dp.theta(i,j) > (-180 + obj.w + obj.frontOffset) ...
-                                && d(i,j) < obj.r3 && abs(abs(dp.theta(i,j)) - obj.frontOffset) == sorted(min_i)
+                        if j ~= i
                             index = index + 1;
-                            front4(index) = j;
-                            %increment min index -> index of next frontmost dp.theta
-                            min_i = min_i + 1;
+                            temp(index) = diff_p(j);
                         end
                     end
+                    diff_p = temp;
+                    sorted = sort(diff_p);
+                    %sorted = sort(d(i,:));
+                    
+                    index = 0;
+                    min_i = 1; %index into sorted array that yields smallest d
+                    for k = 1:N-1
+                        for j = 1:N
+                            %remove dead zone, searching zone, and self, and limit to 4
+                            if index < 4 && j ~= i && abs(dp.theta(i,j)) < (180 - obj.w)...
+                                    && d(i,j) < obj.attractionDistance && d(i,j) == sorted(min_i)
+                                index = index + 1;
+                                closest4(index) = j;
+                                %increment min index -> index of next smallest d
+                                min_i = min_i + 1;
+                            end
+                        end
+                    end
+                    
+                    theta_old = rad2deg(atan2(obj.fishes(i).velocityReal(2),obj.fishes(i).velocityReal(1)));
+                    
+                    if index > 0 %at least 1 neighbor sensed
+                        beta_avg = npi2pi(sum(beta(i,closest4(1:index)))/index);
+                        %add randomness (normal distribution)
+                        sigma = 15; %standard deviation = 15 degrees
+                        alpha = beta_avg + sigma*randn(1);
+                        
+                        theta_new = npi2pi(theta_old + alpha);
+                        
+                    else %no neighbors in sensory range -> searching
+                        beta_avg = npi2pi(360*rand);
+                        alpha = beta_avg; %no need to add normal dist.
+                        theta_new = npi2pi(theta_old + alpha);
+                    end
+                    
+                    velocityReal = (obj.driftSpeed + obj.dv*randn(1)) * ...
+                        [cosd(theta_new) sind(theta_new)];
                 end
-                %         for k = 1:N
-                %             if index < 4 && k ~= i && abs(dp.theta(i,j)) < (180 - w)...
-                %                 && d(i,j) < r3 && abs(dp.theta(i,j)) == sorted(k)
-                %             front4(index + 1) = j;
-                %             index = index + 1;
-                %         end
                 
-                theta_old = rad2deg(atan2(obj.fishes(i).velocityReal(2),obj.fishes(i).velocityReal(1)));
-                
-                if index > 0 %at least 1 neighbor visible
-                    beta_avg = npi2pi(sum(beta(i,front4(1:index)))/index);
-                    
-                    %add randomness (normal distribution)
-                    sigma = 15; %standard deviation = 15 degrees
-                    alpha = beta_avg + sigma*randn(1);
-                    
-                    %calculate velocityReal vector
-                    theta_new = npi2pi(theta_old + alpha);
-                    
-                    
-                else %no neighbors in sight -> searching
-                    beta_avg = npi2pi(360*rand);
-                    alpha = beta_avg; %no need to add normal dist.
-                    
-                    theta_new = npi2pi(theta_old + alpha);
-                    
-                end
-                velocityReal = obj.driftSpeed*[cosd(theta_new) sind(theta_new)];
-                %end
-                
-                %velocityReal = gamrnd(4,1/3.3)*[cosd(theta_new) sind(theta_new)];
-                
-                obj.fishes(i).updatePosition(velocityReal,predatorPosition);
+                obj.fishes(i).updatePosition(velocityReal, predatorPosition);
                 
             end
-            
             
         end
         
