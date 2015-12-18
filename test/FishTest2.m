32dc8424a966b291c295b338db15ec70d343563f
Andreas Magnusson <andreas.magnusson90@gmail.com>
2015-12-18 18:30:25 +0100

    Changed made when making new FishShoal class.

--- test/FishTest2.m
+++ test/FishTest2.m
@@ -3,25 +3,25 @@ clear all
 
 %parameters
 N = 30; %school size
-T = 100; %tank size
+T = 200; %tank size
 
 
 % Initialize fishes and calculator
-fishShoal = FishShoal(T, N, 2, 3, 10);
+fishShoal = FishShoal(T, N, 2, 3, 10, 20);
 
 trapPos = [80 50];
 
-%draw;
+draw;
 
 
 
 % Simulate
 while(1)
-    tic
-    fishShoal.updateFishes(trapPos);
-    toc
     
-    pause(10)
+    fishShoal.updateFishes(trapPos);
+    
+    
+    %pause(10)
     draw;
     
 end
