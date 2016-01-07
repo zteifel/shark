e2251ae62aa5d2320127f9d8de8de593cabea75d
Henrik Adolfsson <zteifel@gmail.com>
2016-01-07 13:02:03 +0100

    remove print from Aquarium

--- code/Aquarium.m
+++ code/Aquarium.m
@@ -37,8 +37,7 @@ classdef Aquarium < handle
             obj.updateFig();
           end
       end
-      distToFish = (obj.shark.distToFish/count)^2;
-      distToFish
+      distToFish = (obj.shark.distToFish/count);
       fitness = (obj.shark.fishEaten+distToFish)/obj.shark.maxEnergy;
     end
 
